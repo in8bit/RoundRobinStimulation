0b8c0a66da7f0980952c9c77935cfebf94f970c1
Nidhi Shah <nidhibaidshah@gmail.com>
2020-09-29 12:52:44 -0400

    complete phase 1. all user input with all the test cases done.

--- src/RoundRobinStimulation.java
+++ src/RoundRobinStimulation.java
@@ -6,9 +6,12 @@ import java.util.List;
 import java.util.Scanner;
 
 public class RoundRobinStimulation<Static> {
+    static List<String> str = new ArrayList<String>();
+    private static final Scanner userInput = new Scanner(System.in);
 
-    public static void main(String[] args) throws FileNotFoundException {
-        // File outputFile = getOutputFile();                          //first command takes the output file to write the 5 unit progress to
+    public static void main(String[] args) {
+
+        File outputFile = getOutputFile();                             //first command takes the output file to write the 5 unit progress to
         File inputFile = getInputFile();                               //second command gets input file to read the processes from
         if (!inputFileChecked(inputFile)) {
             System.out.println("Error: please check the data provided in the file. It must be all integers with value more than 0." +
@@ -17,20 +20,24 @@ public class RoundRobinStimulation<Static> {
                     " Stimulation ends now.");
             System.exit(0);
         }
-        // File outputLOGFile = getOutputFile();                       //third command gets the output log file, where the data from every time unit is to be written
-//        Scanner userInput = new Scanner(System.in);
-//        System.out.println("Enter a time Quantum.");                //fourth command gets the quantum for the stimulation
-//        int quantum = userInput.nextInt();
-//        if (quantum <= 0) {
-//            System.out.println("Please enter a quantum number more than 0.");
-//        }
-
+        System.out.println("*Output Log file*");
+        File outputLOGFile = getOutputFile();                       //third command gets the output log file, where the data from every time unit is to be written
+        System.out.println("Enter a time Quantum.");                //fourth command gets the quantum for the stimulation
+        int quantum = userInput.nextInt();
+        while (quantum <= 0) {
+            System.out.println("Please enter a quantum number more than 0.");
+            quantum = userInput.nextInt();
+        }
+        System.out.println("************************");
+        System.out.println("quantum : " + quantum);
+        for (int i = 0; i < str.size(); i++) {
+            System.out.println("job : " + str.get(i));
+        }
     }
 
     public static File getOutputFile() {
         File file = null;
         String input = "";
-        Scanner userInput = new Scanner(System.in);
         try {
             do {
                 System.out.println("Enter a name of the output file that doesnt exist.");
@@ -40,27 +47,23 @@ public class RoundRobinStimulation<Static> {
         } catch (IOException e) {
             e.printStackTrace();
         }
-        userInput.close();
         return file;
     }
 
     public static File getInputFile() {
         String input = "";
         File file = null;
-        Scanner userInput = new Scanner(System.in);
         while (!new File(input + ".txt").exists()) {
             System.out.println("Enter Input file name.");
             input = userInput.nextLine().replaceAll("\\s+", "");
         } //while loops makes sure to get the name of a file that exists
         file = new File(input + ".txt");
-        userInput.close();
         return file;
 
     }
 
     public static boolean inputFileChecked(File file) {
         Scanner scan = null;
-        List<String> str = new ArrayList<String>();
         try {
             scan = new Scanner(file);
         } catch (FileNotFoundException e) {
@@ -84,7 +87,6 @@ public class RoundRobinStimulation<Static> {
                 int jobLength = (subStr.length - 2);                            // job length = total-(timeofArrival +CpuBursts)
                 if (jobLength != (totalCpuBursts * 2) - 1)
                     return false;          //num of CPU bursts mentioned are not provided then data is invalid
-
             }
             scan.close();
             return true;
